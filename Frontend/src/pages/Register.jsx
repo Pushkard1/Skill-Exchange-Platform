4b599adbb7283e59f0937f291f32c3b2e89e91c9
Pushkar <pushkardeshmukh62@gmail.com>
2024-01-15 13:01:27 +0530

    pages

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function Register() {
   return (
-    <div>Register</div>
+    <div>
+      <form>
+        <label>Name</label>
+        <input type='text' placeholder='Enter your name......'/>
+        <label>Email</label>
+        <input type='email' placeholder='Enter your EmailID......'/>
+        <label>Password</label>
+        <input type='password' placeholder='Enter your password......'/>
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
   )
 }
